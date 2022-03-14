b9d126a9de558ec5bcc004a7e1b634f11a18b99a
Richard Hone <richard@engineeringrh.com>
2022-03-14 18:55:18 -0400

    Initial commit

---
+++ miniproject.js
@@ -0,0 +1,49 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Students = /** @class */ (function () {
+    function Students(name, age, gender, nationality, gpa) {
+        this.name = name;
+        this.age = age;
+        this.gender = 'female';
+        this.nationality = nationality;
+        this.gpa = gpa;
+    }
+    Students.prototype.displayStudent = function () {
+        console.log("This person is ".concat(this.gender));
+    };
+    Students.prototype.getNationality = function () {
+        console.log("The ".concat(this.gender, " student, ").concat(this.name, ", aged ").concat(this.age, ", is from ").concat(this.nationality, " with a GAP of ").concat(this.gpa, "."));
+    };
+    return Students;
+}());
+var UnderGraduates = /** @class */ (function (_super) {
+    __extends(UnderGraduates, _super);
+    function UnderGraduates(name, age, gender, nationality, gpa, batch) {
+        var _this = _super.call(this, name, age, gender, nationality, gpa) || this;
+        _this.batch = batch;
+        return _this;
+    }
+    UnderGraduates.prototype.showUndergrad = function () {
+        console.log("The ".concat(this.gender, " student, ").concat(this.name, ", aged ").concat(this.age, ", is from ").concat(this.nationality, " with a GPA of ").concat(this.gpa, " from batch ").concat(this.batch, "."));
+    };
+    return UnderGraduates;
+}(Students));
+var student1 = new Students("Sandra Kid", 21, '', "France", 4.5);
+student1.displayStudent();
+student1.getNationality();
+var underGrad1 = new UnderGraduates("James Tin", 23, 'male', "Germany", 3.6, 1234);
+underGrad1.displayStudent;
+underGrad1.showUndergrad();
---
+++ miniproject.ts
@@ -0,0 +1,53 @@
+
+interface Iundergrad{
+     name: string;
+     age: number;
+     gender: 'female';
+     nationality: string;
+     gpa: number;
+}
+
+class Students implements Iundergrad {
+    
+     name: string;
+     age: number;
+     gender: 'female';
+     nationality: string;
+     gpa: number;
+     
+     constructor(name: string, age:number, gender: string, nationality:string, gpa:number) {
+         this.name = name;
+         this.age=age;
+          this.gender = 'female';
+          this.nationality = nationality;
+          this.gpa = gpa;
+     }
+ 
+     displayStudent(): void {
+         console.log(`This person is ${this.gender}`);
+     }
+     getNationality(): void{
+          console.log(`The ${this.gender} student, ${this.name}, aged ${this.age}, is from ${this.nationality} with a GAP of ${this.gpa}.`)
+     }
+}
+
+
+
+class UnderGraduates extends Students {
+     batch: number;
+     constructor(name: string, age:number, gender: string, nationality:string, gpa:number, batch:number) {
+          super(name, age, gender, nationality, gpa);
+          this.batch = batch;
+     }
+     showUndergrad(): void{
+          console.log(`The ${this.gender} student, ${this.name}, aged ${this.age}, is from ${this.nationality} with a GPA of ${this.gpa} from batch ${this.batch}.`)
+     }
+}
+
+var student1: Students = new Students("Sandra Kid", 21, '', "France", 4.5);
+student1.displayStudent();
+student1.getNationality();
+
+var underGrad1: UnderGraduates = new UnderGraduates("James Tin", 23, 'male', "Germany", 3.6, 1234);
+underGrad1.displayStudent;
+underGrad1.showUndergrad();
